674461197172b753f36a42fe11ffe083d10024e6
Arthur Kalikiti <arthurkalikiti@gmail.com>
2024-09-24 08:06:45 +0200

    feat: Added password to cloud_confgi

--- clusters/serverless/operations.go
+++ clusters/serverless/operations.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 
 	"github.com/spf13/viper"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/watch"
 )
 
@@ -14,16 +16,45 @@ type Serverless struct {
 
 func NewCluster() *Serverless {
 	return &Serverless{
-		kubeconfig: viper.GetString("virtual_machines.kubeconfig"),
+		kubeconfig: viper.GetString("serverless_cluster.kubeconfig"),
 	}
 }
 
 func (c *Serverless) Create(resource clusters.ClusterResource) error {
-	return errors.New("not implemented")
+	obj := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "serving.knative.dev/v1",
+			"kind":       "Service",
+			"metadata": map[string]interface{}{
+				"name": resource.Compute.Name,
+			},
+			"spec": map[string]interface{}{
+				"template": map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"annotations": map[string]interface{}{
+							"autoscaling.knative.dev/min-scale": "0",
+							"autoscaling.knative.dev/max-scale": resource.Compute.Instances,
+						},
+					},
+					"spec": map[string]interface{}{
+						"containers": map[string]interface{}{
+							"image": resource.Compute.URL,
+						},
+					},
+				},
+			},
+		},
+	}
+	_, err := clusters.CreateResourceSchema(obj, c.kubeconfig, resource.Namespace)
+	return err
 }
 
 func (c *Serverless) Delete(resource clusters.ClusterResource) error {
-	return errors.New("not implemented")
+	return clusters.DeleteResourceSchema(schema.GroupVersionKind{
+		Group:   "serving.knative.dev",
+		Version: "v1",
+		Kind:    "Service",
+	}, resource.Compute.Name, c.kubeconfig, resource.Namespace)
 }
 
 func (c *Serverless) Patch(resource clusters.ClusterResource) (map[string]interface{}, error) {
@@ -31,15 +62,39 @@ func (c *Serverless) Patch(resource clusters.ClusterResource) (map[string]interf
 }
 
 func (c *Serverless) Watch(resource clusters.ClusterResource) (watch.Interface, error) {
-	return nil, errors.New("not implemented")
+	return clusters.WatchResourceSchema(schema.GroupVersionKind{
+		Group:   "serving.knative.dev",
+		Version: "v1",
+		Kind:    "Service",
+	}, c.kubeconfig, resource.Namespace)
 }
 
 func (c *Serverless) Find(resource clusters.ClusterResource) (map[string]interface{}, error) {
-	return nil, errors.New("not implemented")
+	svc, err := clusters.GetResourceSchema(schema.GroupVersionKind{
+		Group:   "serving.knative.dev",
+		Version: "v1",
+		Kind:    "Service",
+	}, resource.Compute.Name, c.kubeconfig, resource.Namespace)
+	if err != nil {
+		return nil, err
+	}
+	return svc.Object, nil
 }
 
 func (c *Serverless) FindAll(resource clusters.ClusterResource) ([]map[string]interface{}, error) {
-	return nil, errors.New("not implemented")
+	svcs, err := clusters.ListResourceSchema(schema.GroupVersionKind{
+		Group:   "serving.knative.dev",
+		Version: "v1",
+		Kind:    "Service",
+	}, c.kubeconfig, resource.Namespace)
+	if err != nil {
+		return nil, err
+	}
+	result := make([]map[string]interface{}, len(svcs.Items))
+	for i, item := range svcs.Items {
+		result[i] = item.Object
+	}
+	return result, nil
 }
 
 func (c *Serverless) Update(resource clusters.ClusterResource) (map[string]interface{}, error) {
